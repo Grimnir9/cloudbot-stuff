847838972d60dec8fc16f721b075073d06c1dc33
Grimnir9 <grimnir@grimnir.xyz>
2017-06-26 11:11:51 -0600

    Update print_url_title.py

--- print_url_title.py
+++ print_url_title.py
@@ -3,8 +3,10 @@ import re
 from bs4 import BeautifulSoup
 from cloudbot import hook
 
-# This will match ANY we url including youtube, reddit, twitch, etc... Some additional work needs to go into
+# This will match ANY we url including reddit, twitch, etc... Some additional work needs to go into
 # not sending the web request etc if the match also matches an existing web regex.
+
+# 2017-06-26 Modified to exclude YouTube urls as they are handled by youtube.py. Also eliminated at link.
 
 url_re = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
