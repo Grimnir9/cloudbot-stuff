b1f22b33a60da2b27ac17090c0fa759e510dd051
Grimnir9 <grimnir@grimnir.xyz>
2017-07-22 13:31:07 -0600

    Create rlmradio.py

---
+++ rlmradio.py
@@ -0,0 +1,23 @@
+#Grabs Current Info for RLM Radio Stream
+
+import requests
+import re
+from cloudbot import hook
+import urllib
+import urllib.request
+
+url = "http://38.135.36.125:7359/7.html"
+
+
+@hook.command("rlmradio", autohelp=False)
+def rlmradio(text):
+
+    url = "http://38.135.36.125:7359/7.html"
+    html = urllib.request.urlopen(url).read()
+    htmlout = html[28:-15]
+    pw_bytes = htmlout.decode("utf-8")
+    filtered = pw_bytes.replace("&apos;", "'")
+    filtered = "Now on the RLM Radio Stream: " + filtered
+    out = filtered
+    return out
+
