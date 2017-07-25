e31a759c913b1f723cca9405d26c0ee59b69f1ba
Grimnir9 <grimnir@grimnir.xyz>
2017-07-25 13:12:00 -0600

    Update rlmradio.py

--- rlmradio.py
+++ rlmradio.py
@@ -1,4 +1,4 @@
-#Grabs Current Info for RLM Radio Stream
+#Grabs Current Info for RLM Radio Stream and others
 
 import requests
 import re
@@ -18,3 +18,41 @@ def rlmradio(text):
     out = filtered
     return out
 
+''' BU4B is blocked
+@hook.command("bu4b", autohelp=False)
+def bu4b(text):
+
+    url = "http://72.13.82.82:5100/7.html"
+    html = urllib.request.urlopen(url).read()
+    htmlout = html[83:-14]
+    pw_bytes = htmlout.decode("utf-8")
+    filtered = pw_bytes.replace("&apos;", "'")
+    filtered = "Now on the Belly Up For Blues Stream: " + filtered
+    out = filtered
+    return out
+'''
+
+@hook.command("aardvark", autohelp=False)
+def aardvark(text):
+
+    url = "http://orion.prostreaming.net:8288/7.html"
+    html = urllib.request.urlopen(url).read()
+    htmlout = html[35:-14]
+    pw_bytes = htmlout.decode("utf-8")
+    filtered = pw_bytes.replace("&apos;", "'")
+    filtered = "Now on the Aardvark Radio Stream: " + filtered
+    out = filtered
+    return out
+
+
+@hook.command("radiocrypto", autohelp=False)
+def radiocrypto(text):
+
+    url = "http://104.131.121.96:8000/7.html"
+    html = urllib.request.urlopen(url).read()
+    htmlout = html[30:-14]
+    pw_bytes = htmlout.decode("utf-8")
+    filtered = pw_bytes.replace("&apos;", "'")
+    filtered = "Now on the http://radiocrypto.com/ Radio Stream: " + filtered
+    out = filtered
+    return out
